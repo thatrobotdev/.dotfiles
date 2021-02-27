5b0b7abe1ece2db1ac0b1bc9689d61a385abf94b
James Kerrane <hi@jameskerrane.com>
2021-02-26 19:00:02 -0700

    Add error checking + quote variable

--- pull.sh
+++ pull.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Color
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
@@ -9,7 +10,7 @@ echo "⚙ Make sure that no ~/.dotfiles folder exists, or that it is empty for t
 echo "⚙ In the future, just re-run the install.sh command using ./install.sh in the ~/.dotfiles folder."
 
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
+if test ! "$(which brew)"; then
     echo -e "${GREEN}⚙ Installing Homebrew${NC}"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 else
@@ -22,6 +23,6 @@ brew install git
 
 git clone https://github.com/thatrobotdev/.dotfiles.git ~/.dotfiles
 
-cd ~/.dotfiles
+cd ~/.dotfiles || echo -e "${RED} 'cd ~/.dotfiles' failed." && exit 1
 
 ./install.sh
